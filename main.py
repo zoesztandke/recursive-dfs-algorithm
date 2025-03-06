6dd4b286d116bce60f2b4cd32d974363b557ff02
Zoe Sztandke <zoe.sztandke@outlook.com>
2025-03-06 16:01:03 +0000

    added comments

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from typing import Dict, Set, Union
 
 def dfs(G: Dict[Union[int, str], Set[Union[int, str]]]) -> None:
-    visited = {node: False for node in G}
+    """
+    Depth-first search on a graph G - recursive implementation
+    :param G: a graph G
+    :return: none
+    """
+    visited = {node: False for node in G} # set all nodes to unvisited initially
 
-    for v in G:
-        if not visited[v]:
-            dfs_from_vertex(G, v, visited)
+    for v in G: # for vertex in G
+        if not visited[v]: # if vertex is not visited
+            dfs_from_vertex(G, v, visited) # call dfs from that vertex
 
 def dfs_from_vertex(G, v, visited) -> None:
-    visited[v] = True
-    for w in v:
-        if not visited[w]:
-            dfs_from_vertex(G, w, visited)
+    """
+    Depth-first search from vertex v
+    :param G: a graph G
+    :param v: starting vertex v
+    :param visited: a list of states for the vertices
+    :return: none
+    """
+    visited[v] = True # mark vertex as visited
+
+    for w in v: # for each vertex w adjacent to v
+        if not visited[w]: # if w is not visited
+            dfs_from_vertex(G, w, visited) # call dfs from w
